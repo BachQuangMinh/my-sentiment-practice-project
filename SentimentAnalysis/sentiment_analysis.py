c94a2d7d321b6a8d7288d221bd933a89cefb45b3
BachQuangMinh <bachquangminh.edu@gmail.com>
2025-09-08 19:35:56 +0700

    complete packaging the backend

---
+++ SentimentAnalysis/sentiment_analysis.py
@@ -0,0 +1,16 @@
+import requests
+
+def sentiment_analyzer(text_to_analyze):
+    ''' This function takes the input text and applies sentiment analysis
+        over it using the deployed model on IBM Cloud. The output returns
+        the sentiment label and its confidence score.
+    '''
+    url = 'https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'
+    headers = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
+    input_json = { "raw_document": { "text": text_to_analyze } }
+    
+    response = requests.post(url, headers=headers, json=input_json)
+    formatted_response = response.json()
+    label = formatted_response['documentSentiment']['label']
+    score = formatted_response['documentSentiment']['score']
+    return {'label': label, 'score': score}
